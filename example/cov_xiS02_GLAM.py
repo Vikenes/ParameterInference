ee59df3e343a9b29a451c90689e514a8b63de925
Vikenes <vetle.vkns@gmail.com>
2024-03-12 14:04:13 +0100

    minor changes for testing purposes

--- example/cov_xiS02_GLAM.py
+++ example/cov_xiS02_GLAM.py
@@ -2,7 +2,8 @@ import numpy as np
 import sys
 import h5py
 import matplotlib
-matplotlib.use('Agg')
+import time 
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
@@ -18,6 +19,7 @@ xi2 = np.zeros([s_bin_centre.shape[0], Nr])
 
 _lst0 = []
 _lst2 = []
+t0 = time.time()
 for jj in range(Nr):
     box_id = int(jj + 1)
     xi0[:, jj] = file_xiS_sim['xi_vg_S'][f'box{box_id}']['xi_vg_S0'][...]
@@ -33,22 +35,27 @@ xi_all[s_bin_centre.shape[0]:s_bin_centre.shape[0]*2] = xi2
 Cov_all = np.cov(xi_all)
 R_all = np.corrcoef(xi_all)
 
-np.save(
-    f'cov_xiS02_GLAM',
-    Cov_all,
-)
-np.save(
-    f'coef_xiS02_GLAM',
-    R_all,
-)
+print(f"Time: {time.time() - t0:.2f} s")  
+# np.save(
+#     f'cov_xiS02_GLAM',
+#     Cov_all,
+# )
+# np.save(
+#     f'coef_xiS02_GLAM',
+#     R_all,
+# )
 
 fig = plt.figure(figsize=(5, 5))
 gs = gridspec.GridSpec(1, 1,)
 ax0 = plt.subplot(gs[0])
 
 ax0.imshow(R_all, cmap='bwr',)
-plt.savefig(
-    "coef_GLAM.pdf",
-    bbox_inches="tight",
-    pad_inches=0.05,
-)
+# plt.savefig(
+    # "coef_GLAM.pdf",
+    # bbox_inches="tight",
+    # pad_inches=0.05,
+# )
+
+# plt.show()
+
+
